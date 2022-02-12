4a09cd91becc88a11a20c3647baa47940e7ee1a4
Marcus Chan <marcusgchan35@gmail.com>
2022-02-12 12:58:54 -0800

    Finished basic layout for quotes page

--- src/components/QuotesPage.js
+++ src/components/QuotesPage.js
@@ -1,7 +1,26 @@
 import React from "react";
+import styles from "./styles/QuotesPage.module.css";
 
 const QuotesPage = () => {
-  return <div>SavedQuotesPage</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Saved Quotes</h1>
+      <section className={styles.inputQuotesContainer}>
+        <input type="text" className={styles.search} />
+        <div className={styles.quotesContainer}>
+          <Row quote="random stuffs here adfadfdsaf dsfa sf " />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const Row = ({ quote, isDuckified }) => {
+  return (
+    <div className={styles.quote}>
+      <p>{quote}</p>
+    </div>
+  );
 };
 
 export default QuotesPage;
