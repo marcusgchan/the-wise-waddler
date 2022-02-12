b23da179005b319c25fd58bb1a2a9a35000d9de6
Kentaro Lim <kentarolim10@gmail.com>
2022-02-12 13:09:24 -0800

    Included Duck Images

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./styles/Homepage.module.css";
+import DuckClosedMouth from "../img/DuckClosedMouth.svg";
+import DuckOpenMouth from "../img/DuckOpenMouth.svg";
 
 const Homepage = () => {
   return <div className={styles.container}>Homepage</div>;
