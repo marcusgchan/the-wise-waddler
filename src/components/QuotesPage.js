b531858eebd0859f1be3622c4f7e18f9d6e65c90
Kentaro Lim <kentarolim10@gmail.com>
2022-02-12 17:03:42 -0800

    Adds filter search and dummy json data to QuotesPage

--- src/components/QuotesPage.js
+++ src/components/QuotesPage.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles/QuotesPage.module.css";
 import NotePad from "../img/NotePad.svg";
+import dummyQuotes from "../dummyQuotes.json";
+import { useNavigate } from "react-router-dom";
 
 const QuotesPage = () => {
+  const quotes = dummyQuotes.quotes;
+  const [filterSearch,setFilterSearch] = useState("");
+  const [filteredQuotes, setFilteredQuotes] = useState(quotes);
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if (!filterSearch.replace(/\s/g, '').length) { //If there is only whitespace in filterSearch
+      setFilteredQuotes(quotes);
+    } else {
+      setFilteredQuotes(quotes.filter((quote)=>quote.noQuackQuote.includes(filterSearch)));
+    }
+  },[filterSearch]);
+
+  const handleFilterChange = (e) => {
+    setFilterSearch(e.target.value);
+  }
+
   return (
     <>
       <div className={styles.container}>
@@ -11,48 +29,37 @@ const QuotesPage = () => {
           <img src={NotePad} alt="" className={styles.notePad} />
         </div>
         <section className={styles.inputQuotesContainer}>
-          <input type="text" className={styles.search} />
+          <input type="text" className={styles.search} value={filterSearch} 
+          onChange={handleFilterChange}/>
           <div className={styles.quotesContainer}>
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
-            <Row quote="random stuffs here adfadfdsaf dsfa sf " />
+            <Rows filteredQuotes={filteredQuotes}/>
           </div>
         </section>
       </div>
       <div className={styles.backButtonContainer}>
-        <a href="/" className={styles.backButton}>GO BACK</a>
+        <button className={styles.backButton} onClick={()=>navigate("/")}>GO BACK</button>
       </div>
     </>
     
   );
 };
 
-// const Rows = () => {
+const Rows = ({filteredQuotes}) => {
+  // const quotes = localStorage.getItem("quotes");
   
-// };
+  return(
+    <div>
+      {filteredQuotes.map((quote)=>
+        <Row key={quote.id} quote={quote}/>)}
+    </div>
+  );
+  
+};
 
 const Row = ({ quote, isDuckified }) => {
   return (
     <div className={styles.quote}>
-      <p>{quote}</p>
+      <p>{quote.noQuackQuote}</p>
     </div>
   );
 };
